9d3ed66cbdc3c6738f7d9642906f0d884b129b04
GkingOficial <talisoncaedoso@gmail.com>
2021-07-18 18:21:05 -0300

    up

--- src/main/java/gking/crow/apirest/ApirestApplication.java
+++ src/main/java/gking/crow/apirest/ApirestApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApirestApplication {
     @GetMapping
     public String getBook(){
-        return "API Test!";
+        return "Testando!";
     }
 
     public static void main(String[] args) {
